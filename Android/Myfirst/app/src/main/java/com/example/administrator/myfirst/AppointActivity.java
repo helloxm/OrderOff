0a6a70c035ac79f82653fd9f5f7752ff4b74f568
limeilin <765915703@qq.com>
2015-03-07 11:01:23 +0800

    list test

--- Android/Myfirst/app/src/main/java/com/example/administrator/myfirst/AppointActivity.java
+++ Android/Myfirst/app/src/main/java/com/example/administrator/myfirst/AppointActivity.java
@@ -2,22 +2,130 @@ package com.example.administrator.myfirst;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.content.Intent;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 public class AppointActivity extends Activity {
+    private EditText classEt = null;
+    private EditText doctorEt = null;
+    private EditText timeEt = null;
+
+    private String TAG = "AppointActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appoint);
+       classEt = (EditText)this .findViewById(R.id.classEt);
+       doctorEt = (EditText)this .findViewById(R.id.doctorEt);
+       timeEt = (EditText)this .findViewById(R.id.timeEt);
+
     }
-    public void onClickok(View view)
+    public void onClickok(View view) {
+        if (SumbitData()) {
+            startActivityForResult(new Intent(AppointActivity.this, StatusActivity.class), 1);
+        }
+    }
+
+    public boolean  SumbitData()
     {
-        startActivityForResult(new Intent(AppointActivity.this, StatusActivity.class), 1);
+        final String strclass = classEt.getText().toString();
+        final String strdoctor = doctorEt.getText().toString();
+        final String strtime = timeEt.getText().toString();
+
+        if (strclass.isEmpty() || strdoctor.isEmpty()|| strtime.isEmpty())  {
+
+            Toast.makeText(this, "错误！", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        new Thread( new Runnable() {
+            @Override
+            public void run() {
+                String TAG = "one";
+                try {
+
+                    // 先封装一个 JSON 对象，使用post发送数据，创建一盒HttpPost对象
+                    JSONObject param = new JSONObject();
+//                    PrintWriter out = new PrintWriter(new OutputStreamWriter(connection.getOutputStream(), nameEt));
+//                    out.print(param);
+
+                    param.put("class", strclass);
+                    param.put("doctor", strdoctor);
+                    param.put("time", strtime);
+
+                    HttpPost request = new HttpPost("http://192.168.2.106/appoint.php");
+
+                    // 绑定到请求 Entry
+                    StringEntity se = new StringEntity(param.toString());
+                    request.setEntity(se);  //设置请求参数setEntity（）
+
+
+                    HttpParams connParams = new BasicHttpParams();
+                    HttpConnectionParams.setConnectionTimeout(connParams, 5 * 1000);
+                    HttpConnectionParams.setSoTimeout(connParams, 5 * 1000);
+                    HttpClient client = new DefaultHttpClient(connParams);
+                    Log.e(TAG, "SEND");
+                    Log.e(TAG, param.toString());
+                    HttpResponse httpResponse = client.execute(request);  // 发送请求，返回HttpResponse
+                    Log.e(TAG, "retrurn---:" + httpResponse.getStatusLine().getStatusCode());
+                    // 得到应答的字符串，这也是一个 JSON 格式保存的数据
+                    String retSrc = EntityUtils.toString(httpResponse.getEntity());
+                    // 生成 JSON 对象
+                    JSONObject result = new JSONObject(retSrc);
+
+
+                    Log.e(TAG, "return :111" + httpResponse.getStatusLine().getStatusCode());
+                    String strRetCode = result.get("ret_code").toString();
+
+                    Log.e(TAG, "return :" + strRetCode);
+                    int iRetCode = Integer.valueOf(strRetCode).intValue();
+                    if (iRetCode == 0) {
+                        Log.e(TAG, "successful");
+                    } else {
+                        Log.e(TAG, "delfault");
+                    }
+
+                } catch (UnsupportedEncodingException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (ClientProtocolException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (JSONException e) {
+
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        return true;
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- Android/Myfirst/app/src/main/java/com/example/administrator/myfirst/EnterActivity.java
+++ Android/Myfirst/app/src/main/java/com/example/administrator/myfirst/EnterActivity.java
@@ -28,18 +28,98 @@ import java.io.UnsupportedEncodingException;
 
 public class EnterActivity extends Activity {
 
-
+    private EditText cardEt= null;
+    private EditText passwordEt = null;
+    private String TAG = "EnterActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter);
+        cardEt = (EditText)this .findViewById(R.id.cardEt);
+
+        passwordEt = (EditText)this .findViewById(R.id.passwordEt);
     }
 
-    public void onClickok2(View view)
-    {
+    public void onClickok2(View view) {
+        if (SumbitData()) {
             startActivityForResult(new Intent(EnterActivity.this, AppointActivity.class), 1);
+        }
     }
+    public boolean  SumbitData() {
+        final String strCard = cardEt.getText().toString();
 
+
+        final String strPassword = passwordEt.getText().toString();
+        if (strCard.isEmpty() || strPassword.isEmpty()) {
+
+            Toast.makeText(this, "错误！", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String TAG = "one";
+                try {
+
+                    // 先封装一个 JSON 对象，使用post发送数据，创建一盒HttpPost对象
+                    JSONObject param = new JSONObject();
+//                    PrintWriter out = new PrintWriter(new OutputStreamWriter(connection.getOutputStream(), nameEt));
+//                    out.print(param);
+
+
+                    param.put("card_id", strCard);
+                    param.put("password", strPassword);
+
+                    HttpPost request = new HttpPost("http://192.168.2.106/enter.php");
+
+                    // 绑定到请求 Entry
+                    StringEntity se = new StringEntity(param.toString());
+                    request.setEntity(se);  //设置请求参数setEntity（）
+
+
+                    HttpParams connParams = new BasicHttpParams();
+                    HttpConnectionParams.setConnectionTimeout(connParams, 5 * 1000);
+                    HttpConnectionParams.setSoTimeout(connParams, 5 * 1000);
+                    HttpClient client = new DefaultHttpClient(connParams);
+                    Log.e(TAG, "SEND");
+                    Log.e(TAG, param.toString());
+                    HttpResponse httpResponse = client.execute(request);  // 发送请求，返回HttpResponse
+                    Log.e(TAG, "retrurn---:" + httpResponse.getStatusLine().getStatusCode());
+                    // 得到应答的字符串，这也是一个 JSON 格式保存的数据
+                    String retSrc = EntityUtils.toString(httpResponse.getEntity());
+                    // 生成 JSON 对象
+                    JSONObject result = new JSONObject(retSrc);
+
+
+                    Log.e(TAG, "return :111" + httpResponse.getStatusLine().getStatusCode());
+                    String strRetCode = result.get("ret_code").toString();
+
+                    Log.e(TAG, "return :" + strRetCode);
+                    int iRetCode = Integer.valueOf(strRetCode).intValue();
+                    if (iRetCode == 0) {
+                        Log.e(TAG, "successful");
+                    } else {
+                        Log.e(TAG, "delfault");
+                    }
+
+                } catch (UnsupportedEncodingException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (ClientProtocolException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (JSONException e) {
+
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        return true;
+    }
     public void onClickok1(View view)
     {
         startActivityForResult(new Intent(EnterActivity.this, RegisterActivity.class), 1);
