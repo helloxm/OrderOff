7204640ca820057e11f3a6c83088dda099ad1af9
helloxm <fangjtan@gmail.com>
2015-04-01 18:18:19 +0800

    修改login，验证用户名，密码不能为空。

--- Web/WebSite/Home/Controller/IndexController.class.php
+++ Web/WebSite/Home/Controller/IndexController.class.php
@@ -8,21 +8,36 @@ class IndexController extends Controller {
 	
 	public function loginAction()
     {
-    	$patient = M('patientinfo', '', 'mysql://root:123456@localhost:3306/offorder#utf8'); 
+    	if (strlen($_POST['user']) <= 0) {
+			echo "<script>{window.alert('用户名不能为空！');location.href='index.html'} </script>";
+			return;
+		}
+		
+    	if (strlen($_POST['password']) <= 0) {
+			echo "<script>{window.alert('密码不能为空！');location.href='index.html'} </script>";
+			return;
+		}
+		
+		$patient = M('patientinfo', '', 'mysql://root:123456@localhost:3306/offorder#utf8'); 
+		$condition['card_id'] = $_POST['user'];
+		$resutdata = $patient->where($condition)->sum('id'); 
+		
+    	if ($resutdata <= 0) {
+			echo "<script>{window.alert('用户不存在！');location.href='index.html'} </script>";
+			return;
+		}
+		
 		$condition['card_id'] = $_POST['user'];
 		$condition['password'] = $_POST['password'];
 		$resutdata = $patient->where($condition)->sum('id'); 
-		//dump($condition);
 		
-		//dump($resutdata);
 		if ($resutdata <= 0) {
-			echo "<script>{window.alert('用户名或者密码错误！');location.href='index.html'} </script>";
+			echo "<script>{window.alert('密码错误！');location.href='index.html'} </script>";
 			return;
 		}
     	
 		$orderinfo = M('orderinfo', '', 'mysql://root:123456@localhost:3306/offorder#utf8'); 
-		
-		$data['name'] = "test";
+		$data['card_id'] = $_POST['user'];
 		$resutdata = $orderinfo->where($condition2)->select();
 
 		$this->assign('list', $resutdata);
